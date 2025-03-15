fa761f90b2dfdc436d1d38b3dd108c5824fcb5c9
Maksym Panov <work.maksym.panov@gmail.com>
2025-03-15 19:22:25 +0200

    Add docker login

--- docker/local/pull.sh
+++ docker/local/pull.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+aws ecr get-login-password --region eu-central-1 --profile default | docker login --username AWS --password-stdin 533267200006.dkr.ecr.eu-central-1.amazonaws.com
+
 docker compose pull
