d700d787d5b640ae9f9da8e76b2e49474c64dd65
Maksym Panov <work.maksym.panov@gmail.com>
2025-03-15 19:31:20 +0200

    Rollback

--- docker/local/pull.sh
+++ docker/local/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker login -u AWS --password-stdin $(aws ecr get-login-password --region eu-central-1) 533267200006.dkr.ecr.eu-central-1.amazonaws.com
+docker login -u AWS -p $(aws ecr get-login-password --region eu-central-1) 533267200006.dkr.ecr.eu-central-1.amazonaws.com
 
 docker compose pull
