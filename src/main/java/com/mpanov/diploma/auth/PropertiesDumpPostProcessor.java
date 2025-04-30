d7ee87019343ff9b9967d5a3a0b21203bbfc0966
Maksym Panov <work.maksym.panov@gmail.com>
2025-04-30 19:34:47 +0300

    Debug properties 3

--- src/main/java/com/mpanov/diploma/auth/PropertiesDumpPostProcessor.java
+++ src/main/java/com/mpanov/diploma/auth/PropertiesDumpPostProcessor.java
@@ -1,26 +1,23 @@
-package com.mydomain.debug;
+package com.mpanov.diploma.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class PropertiesDumpPostProcessor implements EnvironmentPostProcessor {
-    private static final Logger log = LoggerFactory.getLogger(PropertiesDumpPostProcessor.class);
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication app) {
-        log.info("=== BEGIN ENVIRONMENT DUMP ===");
+        System.err.println("=== BEGIN ENVIRONMENT DUMP ===");
         for (var ps : env.getPropertySources()) {
             if (ps instanceof EnumerablePropertySource<?> eps) {
                 for (String key : eps.getPropertyNames()) {
                     String val = env.getProperty(key);
-                    log.info("{} = {}", key, val);
+                    System.err.println(key + " = " + val);
                 }
             }
         }
-        log.info("=== END ENVIRONMENT DUMP ===");
+        System.err.println("=== END ENVIRONMENT DUMP ===");
     }
 }
